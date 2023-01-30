cb9f9bca0b483bd54b9154ca674e1e44a0faf966
Kurt Griebel <kurtgriebel@yahoo.com>
2023-01-30 12:27:04 -0500

    making sure databases are updated correctly

--- capstone/java/database/dropdb.sql
+++ capstone/java/database/dropdb.sql
@@ -5,9 +5,9 @@
 -- The following line terminates any active connections to the database so that it can be destroyed
 SELECT pg_terminate_backend(pid)
 FROM pg_stat_activity
-WHERE datname = 'climbing_db';
+WHERE datname = 'car_repair_db';
 
-DROP DATABASE climbing_db;
+DROP DATABASE car_repair_db;
 
 DROP USER final_capstone_owner;
 DROP USER final_capstone_appuser;
