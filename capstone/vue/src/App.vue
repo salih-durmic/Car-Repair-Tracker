4a385474b39dc7f1770513a7feda877435207082
Jason <milby1996@yahoo.com>
2023-02-03 10:18:05 -0500

    home auth

--- capstone/vue/src/App.vue
+++ capstone/vue/src/App.vue
@@ -2,7 +2,16 @@
   <div id="app">
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link
+        v-bind:to="{ name: 'login' }"
+        v-if="$store.state.token === ''"
+        >Login</router-link
+      >&nbsp;|&nbsp;
+      <router-link
+        v-bind:to="{ name: 'logout' }"
+        v-if="$store.state.token != ''"
+        >Logout</router-link
+      >
     </div>
     <router-view />
   </div>
