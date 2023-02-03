eb2167f03571892c3916d1ef7e6f96caf269897c
Jason <milby1996@yahoo.com>
2023-02-03 10:27:08 -0500

    added emp to user table

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -6,4 +6,7 @@ VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','B
 INSERT INTO users (username,password_hash,first_name,last_name,email_address,phone_number,role)
 VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','Ann', 'Anna','ann@carrepair.com','567-567-5678','ROLE_ADMIN');
 
+INSERT INTO users (username,password_hash,first_name,last_name,email_address,phone_number,role)
+VALUES ('emp','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'Jim', 'Jimbob', 'jim@carrepair.com', '234-345-3456','ROLE_EMPLOYEE');
+
 COMMIT TRANSACTION;
