d4eafe4490248e14e1394001f8cf84619ed1d5bb
Kurt Griebel <kurtgriebel@yahoo.com>
2023-02-08 10:45:34 -0500

    changed the name of Employee History to Completed Orders

--- capstone/vue/src/views/CompletedOrders.vue
+++ capstone/vue/src/views/CompletedOrders.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-      <h1>Employee History</h1>
+      <h1>Completed Orders</h1>
   </div>
 </template>
 
 <script>
 export default {
-    name: "employeehistory",
+    name: "completedOrders",
 
 }
 </script>
